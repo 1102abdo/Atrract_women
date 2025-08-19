3f5cbbd7643ae8e9287d685f770538ba0e96c9ba
1102abdo <ahmeabdallah695@gmail.com>
2025-08-19 03:07:38 +0300

    Add DB check route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
+Route::get('/db-check', function () {
+    try {
+        DB::connection()->getPdo();
+        return "✅ Database connected successfully!";
+    } catch (\Exception $e) {
+        return "❌ Database connection error: " . $e->getMessage();
+    }
+});
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
